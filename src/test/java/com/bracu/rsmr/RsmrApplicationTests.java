af0ee5235f71e47c2427e0b0e4dc7338878ddd50
riadkabir45 <riadkabir45@gmail.com>
2024-12-07 23:23:04 +0600

    feat: base project

---
+++ src/test/java/com/bracu/rsmr/RsmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bracu.rsmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
