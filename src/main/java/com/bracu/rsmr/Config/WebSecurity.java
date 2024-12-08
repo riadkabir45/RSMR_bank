4ab3d12c20a00f9bf48c569e5b2ba2325043f38d
riadkabir45 <riadkabir45@gmail.com>
2024-12-08 12:35:55 +0600

    feat: basic login page

--- src/main/java/com/bracu/rsmr/Config/WebSecurity.java
+++ src/main/java/com/bracu/rsmr/Config/WebSecurity.java
@@ -1,26 +1,44 @@
 package com.bracu.rsmr.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.bracu.rsmr.User.UserLogService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
+    @Autowired
+    private UserLogService userLogService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
         http
+        .userDetailsService(userLogService)
         .csrf(csrfmgr -> {
             csrfmgr.disable();
         })
         .authorizeHttpRequests((authz) -> 
         {
-            authz.anyRequest().permitAll();
+            authz.requestMatchers("/login").permitAll();
+            authz.anyRequest().authenticated();
         }
-        );
+        ).formLogin(logmgr -> {
+            logmgr.permitAll();
+
+        });
 
         return http.build();
     }    
---
+++ src/main/java/com/bracu/rsmr/User/UserLogService.java
@@ -0,0 +1,26 @@
+package com.bracu.rsmr.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserLogService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Username was not found: "+username));
+        
+        return org.springframework.security.core.userdetails.User.builder()
+            .username(user.getUsername())
+            .password(user.getPassword())
+            .build();
+    }
+
+}
