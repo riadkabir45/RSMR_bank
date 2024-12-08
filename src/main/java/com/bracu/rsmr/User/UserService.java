f19c487f4c8e904917c6864fbd22e65ed7de5576
riadkabir45 <riadkabir45@gmail.com>
2024-12-08 21:07:49 +0600

    feat: account attachment

--- src/main/java/com/bracu/rsmr/User/UserService.java
+++ src/main/java/com/bracu/rsmr/User/UserService.java
@@ -6,17 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.bracu.rsmr.Account.Account;
+import com.bracu.rsmr.Account.AccountRepository;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     public void createUser(User user){
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userRepository.save(user);    
+        userRepository.save(user);
+        if(user.getRoles().contains("Customer")){
+              Account account = new Account(1000D,false, (long)((Math.random() * 100) + 1));
+              account.setUser(user);
+              user.setAccount(account);
+              accountRepository.save(account);
+        }
     }
 
     public boolean checkUser(User user){
