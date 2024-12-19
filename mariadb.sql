c5b13e52b39cb4b9e2135926842fdb05c262f436
riadkabir45 <riadkabir45@gmail.com>
2024-12-20 05:25:18 +0600

    fix: wrong host

--- mariadb.sql
+++ mariadb.sql
@@ -1,3 +1,3 @@
 CREATE USER IF NOT EXISTS 'rsmr'@'localhost' IDENTIFIED BY 'spring';
 CREATE DATABASE IF NOT EXISTS `rsmr`;
-GRANT ALL PRIVILEGES ON `rsmr`.* TO 'rsmr'@'your_hostname' IDENTIFIED BY 'spring';
+GRANT ALL PRIVILEGES ON `rsmr`.* TO 'rsmr'@'localhost' IDENTIFIED BY 'spring';
